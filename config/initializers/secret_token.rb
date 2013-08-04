2cc8bba4bde6909f0658f448dc02b0224617867a
openstake <openstake@gmail.com>
2013-08-04 13:03:25 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '489430261c115eded4e5f673406600d60cc1d9ee31cd563b8e0ff8947cfcb7fba676a9ec0514f85c5c4fe4372469ca5ccc7cc9a60c3fe6a5a48b4e4a2f56e61f'
